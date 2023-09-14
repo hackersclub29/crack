cf03b168b9e8b959c68696622ba32d360a5dd5e4
hackersclub29 <57355685+hackersclub29@users.noreply.github.com>
2023-09-14 07:48:02 +0530

    UCrack.py

--- Crack.py
+++ Crack.py
@@ -1,50 +1,102 @@
 import argparse
 import hashlib
+import os
+import threading
+import time
 
-# Create a new ArgumentParser object
-parser = argparse.ArgumentParser(description="Find the password of a hashed value using a word list")
+def find_password(hash_value, wordlist_path, hash_type):
+    if hash_type == "md5":
+        hash_func = hashlib.md5
+        hash_length = 32
+    elif hash_type == "sha1":
+        hash_func = hashlib.sha1
+        hash_length = 40
+    elif hash_type == "sha256":
+        hash_func = hashlib.sha256
+        hash_length = 64
+    else:
+        print("Error: unsupported hash type")
+        return
 
-# Add a "hash" argument that specifies the hash to find the password of
-parser.add_argument("hash", help="The hashed value to find the password of")
+    if len(hash_value) != hash_length:
+        print("Error: Hash length doesn't match the specified hash type")
+        return
 
-# Add a "wordlist" argument that specifies the path to the word list
-parser.add_argument("wordlist", help="The path to the word list file")
+    if not os.path.isfile(wordlist_path):
+        print("Error: Wordlist file not found")
+        return
 
-# Parse the command-line arguments
-args = parser.parse_args()
+    with open(wordlist_path, "r") as wordlist_file:
+        for word in wordlist_file:
+            word = word.strip()
+            hashed_word = hash_func(word.encode()).hexdigest()
+            if hashed_word == hash_value:
+                print("Password found:", word)
+                return
 
-# Verify which hash the user entered
-if len(args.hash) == 32:
-    hash_type = "md5"
-elif len(args.hash) == 40:
-    hash_type = "sha1"
-elif len(args.hash) == 64:
-    hash_type = "sha256"
-else:
-    # If the hash has an unsupported length, exit the program
-    print("Error: unsupported hash type")
-    exit()
+    print("Password not found in word list")
 
-# Open the word list file
-with open(args.wordlist, "r") as wordlist_file:
-    # Read each line in the file (i.e. each word in the list)
-    for word in wordlist_file:
-        # Strip the newline character from the end of the word
-        word = word.strip()
+def crack_password(hash_file, wordlist_file, hash_type, num_threads, processed_hashes):
+    if not os.path.isfile(hash_file):
+        print("Error: Hash file not found")
+        return
 
-        # Hash the word using the same hash function as the provided hash
-        if hash_type == "md5":
-            hashed_word = hashlib.md5(word.encode()).hexdigest()
-        elif hash_type == "sha1":
-            hashed_word = hashlib.sha1(word.encode()).hexdigest()
-        elif hash_type == "sha256":
-            hashed_word = hashlib.sha256(word.encode()).hexdigest()
+    threads = []
 
-        # Compare the hashed word to the provided hash
-        if hashed_word == args.hash:
-            # If they match, print the password and exit the program
-            print("Password found:", word)
-            exit()
+    def crack_hashes(hash_lines):
+        for line in hash_lines:
+            hash_value = line.strip()
+            if hash_value not in processed_hashes:
+                find_password(hash_value, wordlist_file, hash_type)
+                processed_hashes.add(hash_value)
+                print("Cracking hash:", hash_value)
 
-# If the program reaches this point, it means no password was found
-print("Password not found in word list")
+    with open(hash_file, "r") as hashfile:
+        hash_lines = hashfile.readlines()
+        chunk_size = len(hash_lines) // num_threads
+
+        for i in range(num_threads):
+            start = i * chunk_size
+            end = start + chunk_size if i < num_threads - 1 else len(hash_lines)
+            thread = threading.Thread(target=crack_hashes, args=(hash_lines[start:end],))
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+def main():
+    parser = argparse.ArgumentParser(description="Crack hashed passwords using a wordlist and multi-threading")
+    parser.add_argument("wordlist_file", help="The path to the wordlist file")
+    parser.add_argument("--hash-type", choices=["md5", "sha1", "sha256"], default="md5", help="Specify the hash type (default: md5)")
+    parser.add_argument("--threads", type=int, default=1, help="Specify the number of threads to use (default: 1)")
+    parser.add_argument("--hash-file", help="The path to the hash file (default: hash.txt)")
+    parser.add_argument("--wordlist-path", help="The path to the wordlist file (default: wordlist.txt)")
+
+    args = parser.parse_args()
+
+    hash_file = args.hash_file if args.hash_file else "hash.txt"
+    wordlist_file = args.wordlist_path if args.wordlist_path else "wordlist.txt"
+
+    processed_hashes = set()
+
+    while True:
+        with open(hash_file, "r") as hashfile:
+            hash_lines = hashfile.readlines()
+
+        if not hash_lines:
+            print("No more hashes found in", hash_file)
+            break
+
+        # Remove hashes that have already been processed
+        new_hash_lines = [line for line in hash_lines if line.strip() not in processed_hashes]
+        if not new_hash_lines:
+            print("All hashes have been processed.")
+            break
+
+        crack_password(hash_file, wordlist_file, args.hash_type, args.threads, processed_hashes)
+
+        time.sleep(5)  # Adjust the delay (in seconds) between each loop iteration as needed
+
+if __name__ == "__main__":
+    main()
